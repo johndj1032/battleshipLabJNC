5a284fe325c991a49720d557567a2be4f0612e2b
johndj1032 <jdjorup@potomacschool.org>
2020-11-11 11:57:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+from random import randint
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-hello
+board = []
+for x in range(6):
+    board.append(["O"] * 6)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-my name is john
+def print_board(board):
+    for row in board:
+        print((" ").join(row))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("Let's play Battleship!")
+print_board(board)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def random_row(board):
+    return randint(0, len(board) - 1)
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+for turn in range(9):
+    print ("Turn"), turn
+    guess_row = int(input("Guess Row:"))
+    guess_col = int(
+        input("Guess Col:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sunk my battleship!")
+        break
+    else:
+        if (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
+            print("Oops, that's not even in the ocean.")
+        elif(board[guess_row][guess_col] == "X"):
+            print("You guessed that one already.")
+        else:
+            print("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+    if turn == 8:
+        print("Game Over")
+    turn =+ 1
+    print_board(board)
