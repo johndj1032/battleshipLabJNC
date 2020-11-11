3ea97e383b239de7daf2d54a0da1249359fffcb0
johndj1032 <jdjorup@potomacschool.org>
2020-11-11 12:07:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from random import randint
 
 board = []
-for x in range(6):
-    board.append(["O"] * 6)
+for x in range(10):
+    board.append(["O"] * 10)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
 def print_board(board):
     for row in board:
         print((" ").join(row))
 
-print("Let's play Battleship!")
+print("Play:")
 print_board(board)
 
 def random_row(board):
@@ -21,24 +19,20 @@ def random_col(board):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-for turn in range(9):
-    print ("Turn"), turn
+for turn in range(13):
     guess_row = int(input("Guess Row:"))
     guess_col = int(
         input("Guess Col:"))
-
     if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk my battleship!")
+        print("Ship Sunk")
         break
     else:
-        if (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
-            print("Oops, that's not even in the ocean.")
+        if (guess_row < 0 or guess_row > 9) or (guess_col < 0 or guess_col > 9):
+            print("Not Valid")
         elif(board[guess_row][guess_col] == "X"):
-            print("You guessed that one already.")
+            print("Same Guess")
         else:
-            print("You missed my battleship!")
+            print("Miss")
             board[guess_row][guess_col] = "X"
-    if turn == 8:
-        print("Game Over")
-    turn =+ 1
+    turn = turn + 1
     print_board(board)
